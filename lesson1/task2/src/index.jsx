2970c910412de1c2807eede4ef3b2ff57c24580d
Antikiok <antikiok@gmail.com>
2021-10-21 14:49:01 +0300

    1

---
+++ lesson1/task2/src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import './index.scss';
+import Dimensions from './Dimensions.jsx';
+
+ReactDOM.render(<Dimensions />, document.getElementById('root'));
