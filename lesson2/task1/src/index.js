6236f75820b3c8c1e9f8d10486686dbd69791716
Antikiok <antikiok@gmail.com>
2021-10-25 11:25:22 +0300

    1

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -29,8 +29,4 @@ const counterReducer = (state = 0, action) => {
 const store = createStore(counterReducer);
 
 store.dispatch(incremet());
-store.dispatch(incremet());
-store.dispatch(incremet());
 store.dispatch(decrement());
-
-console.log(store.getState());
