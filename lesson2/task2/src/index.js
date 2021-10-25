5861a0ed7916bdda3462efc6693ec58924547dfd
Antikiok <antikiok@gmail.com>
2021-10-25 11:41:18 +0300

    1

--- lesson2/task2/src/index.js
+++ lesson2/task2/src/index.js
@@ -17,9 +17,9 @@ export const decrement = () => {
 
 export const counterReducer = (state = 0, action) => {
   switch (action.type) {
-    case 'INCREMENT':
+    case INCREMENT:
       return state + 1;
-    case 'DECREMENT':
+    case DECREMENT:
       return state - 1;
     default:
       return state;
