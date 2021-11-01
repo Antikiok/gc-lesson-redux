70e296adabc56ae497af61cdd8f30a9a4c961893
Antikiok <antikiok@gmail.com>
2021-11-01 12:22:51 +0200

    1

--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -1,40 +1,8 @@
-import './index.scss';
-import store, { increment, decrement, reset } from './store.js';
+import { store, increment, decrement } from './counter';
 
-const resultElem = document.querySelector('.counter__result');
-const incrementBtn = document.querySelector('[data-action="increment"]');
-const resetBtn = document.querySelector('[data-action="reset"');
-const decrementBtn = document.querySelector('[data-action="decrement"');
+store.subscribe(() => console.log(store.getState()));
 
-const onIncrement = () => {
-  store.dispatch(increment());
-};
-
-const onDecrement = () => {
-  store.dispatch(decrement());
-};
-
-const onReset = () => {
-  store.dispatch(reset());
-};
-
-incrementBtn.addEventListener('click', onIncrement);
-decrementBtn.addEventListener('click', onDecrement);
-resetBtn.addEventListener('click', onReset);
-
-store.subscribe(() => {
-  const state = store.getState();
-  const currentValue = state.history.reduce((acc, value) => acc + value, 0);
-
-  if (state.history.length === 0) {
-    return (resultElem.textContent = '');
-  }
-  const result = state.history.map(el => {
-    if (el > 0) {
-      return `+${el}`;
-    }
-    return `${el}`;
-  });
-
-  resultElem.textContent = `${result.join('')} = ${currentValue}`;
-});
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
