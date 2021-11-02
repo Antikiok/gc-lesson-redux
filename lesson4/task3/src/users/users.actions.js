45c6a2f0420116c39289fa67883e89c4d2f0d71a
Antikiok <antikiok@gmail.com>
2021-11-02 20:34:20 +0200

    1

---
+++ lesson4/task3/src/users/users.actions.js
@@ -0,0 +1,9 @@
+export const GO_PREV = 'USERS/GO_PREV';
+export const GO_NEXT = 'USERS/GO_NEXT';
+
+export const goPrev = () => ({
+  type: GO_PREV,
+});
+export const goNext = () => ({
+  type: GO_NEXT,
+});
