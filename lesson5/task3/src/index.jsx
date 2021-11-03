b3a8f9f8cabb8b6328cc9faa6b8df430878475e4
Antikiok <antikiok@gmail.com>
2021-11-03 20:39:01 +0200

    1

--- lesson5/task3/src/index.jsx
+++ lesson5/task3/src/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import { Provider } from 'react-redux';
+
 import App from './App.jsx';
 import store from './store';
-
-const rootElement = document.querySelector('#root');
+import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  rootElement,
+  document.getElementById('root'),
 );
