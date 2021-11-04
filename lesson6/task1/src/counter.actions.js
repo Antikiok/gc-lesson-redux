f405de98d971eb08f5890cddabe9578ce5a9af89
Antikiok <antikiok@gmail.com>
2021-11-04 16:23:41 +0200

    1

--- lesson6/task1/src/counter.actions.js
+++ lesson6/task1/src/counter.actions.js
@@ -2,8 +2,14 @@ export const INCREMENT = 'COUNTER/INCREMENT';
 export const DECREMENT = 'COUNTER/DECREMENT';
 export const RESET = 'COUNTER/RESET';
 
-export const increment = () => ({ type: INCREMENT });
+export const increment = () => ({
+  type: INCREMENT,
+});
 
-export const decrement = () => ({ type: DECREMENT });
+export const decrement = () => ({
+  type: DECREMENT,
+});
 
-export const reset = () => ({ type: RESET });
+export const reset = () => ({
+  type: RESET,
+});
